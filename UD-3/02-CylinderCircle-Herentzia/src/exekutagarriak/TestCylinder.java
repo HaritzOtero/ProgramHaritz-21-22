408317c9ab1971b801a666a0bc378e9a1b9820e9
Haritz Otero <otero.haritz@uni.eus>
2022-01-25 14:22:24 +0100

    das

--- UD-3/02-CylinderCircle-Herentzia/src/exekutagarriak/TestCylinder.java
+++ UD-3/02-CylinderCircle-Herentzia/src/exekutagarriak/TestCylinder.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkg02.cylindercircle.herentzia;
+package exekutagarriak;
+
+import model.Cylinder;
 
 /**
  *
