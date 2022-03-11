435aa5c00df68eb76cefb70d44b5d98ad1c7ca4e
Haritz Otero <otero.haritz@uni.eus>
2022-03-11 13:39:46 +0100

    11/03/2022

---
+++ UD4-DatuenPersistentzia/HiztegiaSQLite/src/programak/Controller.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programak;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author otero.haritz
+ */
+ public class Controller implements ActionListener {
+    private Model model;
+    private View view;
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+        anadirActionListener(this);       
+    }    
+    private void anadirActionListener(ActionListener listener) {
+        //GUIaren konponente guztiei gehitu listenerra
+        view.jButtonTxertatu.addActionListener(listener);        
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String actionCommand = e.getActionCommand();
+        //listenerrak entzun dezakeen eragiketa bakoitzeko. Konponenteek 'actionCommad' propietatea daukate
+        switch (actionCommand) {
+            case "TXERTATU":
+                System.out.println("TXERTATU botoia sakatu duzu");
+                model.insert(view.jTextFieldEuskaraz.getText(), view.jTextFieldGazteleraz.getText());
+        }
+    }
+ }
---
+++ UD4-DatuenPersistentzia/HiztegiaSQLite/src/programak/ProgramaNagusia.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programak;
+
+/**
+ *
+ * @author parra.raul
+ */
+public class ProgramaNagusia {
+
+    public static void main(String[] args) {
+//        connect();
+        SQLiteaKudeatu app = new SQLiteaKudeatu();
+        app.selectAll();
+
+        app.insert("Platanoa", "Platano");
+        app.selectAll();
+
+    }
+}
