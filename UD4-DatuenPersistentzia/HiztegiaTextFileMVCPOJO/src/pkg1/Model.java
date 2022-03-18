59b3d44374d085ef1b0fbc98d4bf611014ff7da6
Haritz Otero <otero.haritz@uni.eus>
2022-03-18 14:23:06 +0100

    18/03/2022

--- UD4-DatuenPersistentzia/HiztegiaTextFileMVCPOJO/src/pkg1/Model.java
+++ UD4-DatuenPersistentzia/HiztegiaTextFileMVCPOJO/src/pkg1/Model.java
@@ -5,6 +5,13 @@
  */
 package pkg1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -18,6 +25,8 @@ import java.util.ArrayList;
  * @author otero.haritz
  */
 public class Model {
+
+    private final String fitxategia = "Hiztegia.csv";
 
     private Connection connect() {
         // SQLite connection string
@@ -47,12 +56,11 @@ public class Model {
             System.out.println(e.getMessage());
         }
     }
-    public String imprimatuString(){
-        String strTerminoak="";
+
+    public String imprimatuString() {
+        String strTerminoak = "";
         String sql = "SELECT id, euskaraz, gazteleraz FROM Terminoak";
-        
-        
-        
+
         try (Connection conn = this.connect();
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(sql)) {
@@ -67,11 +75,12 @@ public class Model {
             System.out.println(e.getMessage());
         }
         return strTerminoak;
-    
+
     }
+
     public String hiztegiaInprimatu() {
         String sql = "SELECT id, euskaraz, gazteleraz FROM Terminoak";
-        ArrayList <Terminoa> terminoak = new ArrayList<>();
+        ArrayList<Terminoa> terminoak = new ArrayList<>();
         try (Connection conn = this.connect();
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(sql)) {
@@ -134,5 +143,57 @@ public class Model {
             System.out.println(e.getMessage());
         }
 
+    }
+
+
+    public void terminoakGorde(String areakoTestua) {
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+        try {
+            outputStream = new PrintWriter(new FileWriter("gordetakoTerminoak.txt"));
+            outputStream.print(areakoTestua);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+
+    public static void terminoakInprimatu() {
+        
+    }
+
+        public String terminoakJaso() throws IOException {
+        String Resultadoterminos = "";
+        String[] terminoBanaketa = new String[3];
+        Terminoa tx = new Terminoa();
+        BufferedReader inputStream = null;
+
+        try {
+            inputStream = new BufferedReader(new FileReader("terminoak.txt"));
+
+            String l;
+            while ((l = inputStream.readLine()) != null) {
+                terminoBanaketa = l.split(",");
+                tx = new Terminoa(Integer.parseInt(terminoBanaketa[0]), terminoBanaketa[1], terminoBanaketa[2]);
+                if (Resultadoterminos.equals("")) {
+                    Resultadoterminos = tx.toString();
+                } else {
+                    Resultadoterminos = Resultadoterminos + "\n" + tx.toString();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return Resultadoterminos;
     }
 }
---
+++ UD4-DatuenPersistentzia/HiztegiaTextFileMVCPOJO/src/pkg1/Terminoa.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+
+/**
+ *
+ * @author otero.haritz
+ */
+public class Terminoa {
+    private int id;
+    private String euskaraz;
+    private String gazteleraz;
+
+    public Terminoa(int id, String euskaraz, String gazteleraz) {
+        this.id = id;
+        this.euskaraz = euskaraz;
+        this.gazteleraz = gazteleraz;
+    }
+
+    public Terminoa(){
+        this.id = 0;
+        this.euskaraz = "";
+        this.gazteleraz = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEuskaraz() {
+        return euskaraz;
+    }
+
+    public void setEuskaraz(String euskaraz) {
+        this.euskaraz = euskaraz;
+    }
+
+    public String getGazteleraz() {
+        return gazteleraz;
+    }
+
+    public void setGazteleraz(String gazteleraz) {
+        this.gazteleraz = gazteleraz;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + euskaraz + " " + gazteleraz;
+    }
+    
+    
+}
